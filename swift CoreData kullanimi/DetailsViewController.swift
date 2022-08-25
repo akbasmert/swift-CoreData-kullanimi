c06dc4cf800725caeff52f22c3998b2a9dd73871
mert <mert.ma346@gmail.com>
2022-08-25 20:45:23 +0300

    kullanıcı arayüzü oluşturuldu

---
+++ swift CoreData kullanimi/DetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsViewController.swift
+//  swift CoreData kullanimi
+//
+//  Created by Mert AKBAŞ on 25.08.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var isimTextField: UITextField!
+    @IBOutlet weak var fiyatTextField: UITextField!
+    @IBOutlet weak var bedenTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+    }
+    
+
+}
