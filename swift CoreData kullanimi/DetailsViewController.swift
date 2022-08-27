380719911345ee7d7807883bc3176ee43a29370d
mert <mert.ma346@gmail.com>
2022-08-27 21:16:57 +0300

    görsel secimi için gereki olan kodları ekledik ve info.plist dosyasına görsel seçerken kullanıcıya gösterilecek açıklamayı ekledik

--- swift CoreData kullanimi/DetailsViewController.swift
+++ swift CoreData kullanimi/DetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var isimTextField: UITextField!
@@ -17,11 +17,35 @@ class DetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // KLAVYE ACILDIĞINDA TEXTFİELD DIŞINA DOKUNULDUĞUNDA KLAVYEYİ KAPATIYOR.
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
         
         view.addGestureRecognizer(gestureRecognizer)// viewin kendisine atadık yani view e dokunulduğunu anlayacak.
+        
+        imageView.isUserInteractionEnabled = true // image view i kullanıcının etkileşime girecği hale getirdik
+        
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))//gorsele tıklanıldığında çalışacak fonksiyonu ekledik.
+        imageView.addGestureRecognizer(imageGestureRecognizer)
 
    
+    }
+    
+    @objc func gorselSec(){
+        
+        let picker = UIImagePickerController() // galeri ve kameradan foto ve video almak için ekledik. class a da iki tane ekleme yaptık yukarıda.
+        picker.delegate = self
+        picker.sourceType = .photoLibrary // görütüyü galeriden sectik camerada olabilir di
+        picker.allowsEditing = true // secilen fotoya edit izni verdik.
+        present(picker, animated: true, completion: nil) // görüntüyü kullanıcıya gösterdik animasyonlu bir şekilde.
+        
+        
+    }
+    
+    
+    // didfinished yazarak bu fonksiyonu getirdik kullanıcı foto seçmini yaptıktan sonra olaacakları işliyoruz.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage // kasting işlemi yaptık kullnıcı foto seçiminden vazgeçerse uygulama çökmeyecek. .original image diyerek orijinal fotoyu aldık bunu değiştirebiliriz
+        self.dismiss(animated: true, completion: nil)
     }
     
 
